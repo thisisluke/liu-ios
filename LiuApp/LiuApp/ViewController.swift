e8abd0cc56bd368b3966dcf6a70ea9e27852098c
luke-liu-gh <luke.liu.au@outlook.com>
2021-09-19 22:28:18 +1000

    create new project

---
+++ LiuApp/LiuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiuApp
+//
+//  Created by Luke Liu on 19/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
